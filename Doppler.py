0044f61952d3d41295c5931ec7d03e3b79063919
Yves Looser <yves.looser@ost.ch>
2025-12-03 14:19:29 +0100

    added vdt

--- Doppler.py
+++ Doppler.py
@@ -1,7 +1,7 @@
 import numpy as np
 import ephem 
 import datetime
-from CalcMoonPos import get_observer
+from CalcMoonPos import get_observer, get_moon_position
 
 ftx = 1.296e9
 c = 299792458 #m/s
@@ -10,9 +10,16 @@ c = 299792458 #m/s
 
 
 def moon_dist(obs):
+    r = 6367127
     moon = ephem.Moon(obs)
     moon.compute(obs)
-    return float(moon.earth_distance) * ephem.meters_per_au
+    az, el = get_moon_position()
+    alpha = 90 - el
+    center_dist = float(moon.earth_distance) * ephem.meters_per_au
+
+    d = np.sqrt(r**2 + center_dist**2 - 2*r*center_dist*np.cos(alpha))
+    print(d)
+    return d
 
 def moon_vel(obs, delta_s = 2.304):
     d = moon_dist(obs)
@@ -24,8 +31,29 @@ def moon_vel(obs, delta_s = 2.304):
     obs2.date = ephem.now() + ephem.second * delta_s
 
     d2 = moon_dist(obs2)
+    v = (d2-d) / delta_s
 
-    return (d2-d) / delta_s
+    obs3 = ephem.Observer()
+    obs3.lat = obs.lat    
+    obs3.lon = obs.lon    
+    obs3.elevation = obs.elevation   
+    obs3.date = ephem.now() + ephem.second * 2
+
+    d3 = moon_dist(obs3)
+
+    obs4 = ephem.Observer()
+    obs4.lat = obs.lat    
+    obs4.lon = obs.lon    
+    obs4.elevation = obs.elevation   
+    obs4.date = ephem.now() + ephem.second * (2+delta_s)
+
+    d4 = moon_dist(obs4)
+
+    v2 = (d4-d3) / delta_s
+
+    vdt = (v2-v) / 2
+
+    return v, vdt
 
 def doppler(f_tx, v_rel):
     c = 299792458 #m/s
@@ -42,8 +70,8 @@ if __name__ == "__main__":
 
     obs = get_observer()
 
-    v = moon_vel(obs, 10)
+    v ,vdt= moon_vel(obs, 10)
 
     frx = doppler(1296000000, v)
 
-    print(v)
+    print(vdt)
